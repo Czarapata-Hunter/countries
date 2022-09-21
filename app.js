d22c50accc685cd992ffb3757830711d3530db51
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-09-21 15:30:23 -0700

    Part D: Display Counts

--- app.js
+++ app.js
@@ -81,6 +81,7 @@ function displayNotifications() {
         // > Part D: Display a message with
         //      - how many items were returned in countries array
         //      - how many total matching countries were in the db
+        notificationDisplay.textContent = `showing ${countries.length} of ${count} matching countries`;
     }
 }
 
