edabbfe2547f4a550809b8ba69522a664b2fdc17
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-09-21 12:58:33 -0700

    Populate Continent Dropdown

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 /* Imports */
 // > Part A: Import `getCountries` from fetch-utils.js
-import { getContinents, getCountries } from './fetch-utils.js';
+import { getCountries } from './fetch-utils.js';
 // > Part B: Import `getContinents` from fetch-utils.js
 import { renderContinentOption, renderCountry } from './render-utils.js';
-
+import { getContinents } from './fetch-utils.js';
 /* Get DOM Elements */
 const notificationDisplay = document.getElementById('notification-display');
 const searchForm = document.getElementById('search-form');
@@ -21,10 +21,12 @@ window.addEventListener('load', async () => {
     // > Part A: call findCountries (with no arguments)
     findCountries();
     // > Part B: await the call to get continents to get the response
-
+    const response = await getContinents();
     // > Part B: Assign to state the:
     //      - error,
     //      - data (to the continents variable)
+    error = response.error;
+    continents = response.data;
 
     if (!error) {
         displayContinentOptions();
@@ -83,5 +85,7 @@ function displayNotifications() {
 function displayContinentOptions() {
     for (const continent of continents) {
         // > Part B: render and append options to select
+        const option = renderContinentOption(continent);
+        continentSelect.append(option);
     }
 }
